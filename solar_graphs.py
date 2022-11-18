3fc08b33c51b651eca096c353144e101cb442f94
RusApostol <krasin0469@gmail.com>
2022-11-18 13:05:35 +0300

    Added better graphs

--- solar_graphs.py
+++ solar_graphs.py
@@ -4,17 +4,36 @@ import pylab
 import numpy as np
 
 def graphs():
+    """
+    Rendering the graphs with data from stats.txt
+    r(t)
+    v(t)
+    v(r)
+    """
     with open("stats.txt", "r") as f:
         a = list(map(lambda x: x.rstrip().split(), f.readlines()))
-    dat = a[-1]
+    v = []
+    r = []
+    t = []
+    for s in a:
+        v.append(float(s[1]))
+        r.append(float(s[0]))
+        t.append(float(s[2]))
+        
+    v = np.array(v)
+    r = np.array(r)
+    t = np.array(t)
+        
     pylab.subplot(3, 1, 1)
-    pylab.scatter(np.array(float(dat[-1])), np.array(float(dat[0])), color='black')
-    #name graph
+    pylab.plot(t, r, color='black')
+    pylab.title(r'$r(t)$')
     
     pylab.subplot(3, 1, 2)
-    pylab.scatter(np.array(float(dat[-1])), np.array(float(dat[1])), color='black')
+    pylab.plot(t, v, color='black')
+    pylab.title(r'$v(t)$')
     
     pylab.subplot(3, 1, 3)
-    pylab.scatter(np.array(float(dat[0])), np.array(float(dat[1])), color='black')
+    pylab.plot(r, v, color='black')
+    pylab.title(r'$v(r)$')
     
     plt.show()
